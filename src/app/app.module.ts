83b7951e645e6dd0bf76383e4338d254c786bbe4
Chua Wei Zhe <chuazhe@gmail.com>
2021-07-13 00:44:22 +0800

    Initialize Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoverPageComponent } from './cover-page/cover-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoverPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
