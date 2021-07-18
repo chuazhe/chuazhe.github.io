e2a605b59279dc264d6ae59f2932986f6048389b
Chua Wei Zhe <chuazhe@gmail.com>
2021-07-18 18:34:33 +0800

    Add education and working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  isScrolled = false;
+
   menuItems = [
     { title: 'Experience', pageUrl: 'experience' },
     { title: 'Education', pageUrl: 'education' },
@@ -13,4 +15,21 @@ export class AppComponent {
     { title: 'Projects', pageUrl: 'projects' },
     { title: 'Contact', pageUrl: 'contact-page' },
   ];
+
+  ngOnInit() {
+    window.addEventListener('scroll', this.scrollEvent, true);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scrollEvent, true);
+  }
+
+  scrollEvent = (event: any): void => {
+    var n = event.srcElement.scrollTop;
+    if (n > 0) {
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  };
 }
